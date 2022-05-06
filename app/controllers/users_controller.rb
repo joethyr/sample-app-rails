d79963efaff6480adf617ca08dc2fd346e09d5d3
joethyr <joethayr@gmail.com>
2022-05-06 08:46:40 -0600

    fix error message prompt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
   end
 
