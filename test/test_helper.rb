e035244871251f6564f3a9c817593360b3fc81df
joethyr <joethayr@gmail.com>
2022-05-06 12:22:28 -0600

    add behavior that logs in new users automatically in signup process

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,11 @@ require "rails/test_help"
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Set up all fixtures in test/fixtures/*.yml.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  # Returns true if a test user is logged in
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
